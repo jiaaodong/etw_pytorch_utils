d39cd1ddb50d2372afe88db8ac3a3b1e260976fa
amp180 <amp180@gmail.com>
2019-01-19 17:42:29 +0000

    Remove dependency on the future package (#3)
    * Update setup.py to remove dependency on the 'future' package.
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from __future__ import division, absolute_import, with_statement, print_function, unicode_literals
 from setuptools import setup, find_packages
-import builtins
+
+
+try:
+    import builtins
+except: 
+    import __builtin__ as builtins
+
 
 builtins.__ETW_PT_UTILS_SETUP__ = True
 import etw_pytorch_utils
@@ -10,4 +16,4 @@ setup(
     version=etw_pytorch_utils.__version__,
     author='Erik Wijmans',
     packages=find_packages(),
-    install_requires=['numpy', 'scipy', 'tqdm', 'visdom', 'future', 'statistics'])
+    install_requires=['numpy', 'scipy', 'tqdm', 'visdom', 'statistics'])
